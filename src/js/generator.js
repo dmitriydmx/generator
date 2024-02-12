17ddfd5877a9424a91c87864bc74fb242fdece51
dmitriydmx <dmitriydmx@mail.ru>
2024-02-12 18:30:21 +0900

    first commit generator

---
+++ src/js/generator.js
@@ -0,0 +1,42 @@
+class Character {
+  constructor(name, type, health, level, attack, defence) {
+    this.name = name;
+    this.type = type;
+    this.health = health;
+    this.level = level;
+    this.attack = attack;
+    this.defence = defence;
+  }
+}
+
+class Team {
+  constructor() {
+    this.characters = [];
+  }
+
+  *[Symbol.iterator]() {
+    for (const character of this.characters) {
+      yield character;
+    }
+  }
+
+  add(character) {
+    if (character instanceof Character) {
+      this.characters.push(character);
+    } else {
+      throw new Error('Only instances of Character can be added to the team');
+    }
+  }
+}
+
+
+const char1 = new Character('Лучник1', 'Bowman', 50, 1, 40, 10);
+const char2 = new Character('Лучник2', 'Bowman', 50, 1, 40, 10);
+
+const team = new Team();
+team.add(char1);
+team.add(char2);
+
+for (let character of team) {
+  console.log(character);
+}
